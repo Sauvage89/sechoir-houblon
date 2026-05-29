5a2c2243e40061ce7ee8af5da0383ba2c92ef29f
Lénny Sauvage <sauvagelenny89@gmail.com>
2026-05-29 10:07:24 +0200

    refactor de la fonction get_temperature

--- sauvage/main.c
+++ sauvage/main.c
@@ -12,8 +12,8 @@ const char	PATH_SENSOR3[] = "sensor3";
 const char	PATH_SENSOR4[] = "sensor4";
 const char	PATH_SENSOR5[] = "sensor5";
 const char	PATH_SENSOR6[] = "sensor6";
-const char	*PATH_SENSORS[] =
-{
+
+const char	*PATH_SENSORS[] = {
 	PATH_SENSOR1,
 	PATH_SENSOR2,
 	PATH_SENSOR3,
@@ -22,38 +22,88 @@ const char	*PATH_SENSORS[] =
 	PATH_SENSOR6
 };
 
-// Code erreur -500
-double	get_temperature(int id_capteur)
+int	ft_strstr(char **str, char *find)
+{
+	*str = strstr(*str, find);
+	if (*str == NULL)
+		return (1);
+	return (0);
+}
+
+int	get_temperature_open_file(char **buffer, int id_capteur)
 {
 	int	fd;
-	int	i;
-	double	temperature;
-	char	*buffer;
 
-	buffer = malloc(512);
-	if (buffer == NULL)
-		return (-500);
 	fd = open(PATH_SENSORS[id_capteur], O_RDONLY);
 	if (fd == -1)
+	{
+		close(fd);
+		free(buffer);
 		return (-501);
-	i = read(fd, buffer, 100);
+	}
+	return (fd);
+}
+int	get_temperature_read_file(char **buffer, int *fd)
+{
+	int	i;
+
+	i = read(*fd, *buffer, 99);
+	close(*fd);
 	if (i == -1)
+	{
+		free(*buffer);
 		return (-502);
-	buffer[i] = '\0';
-	close(fd);
-	buffer = strstr(buffer, "YES");
-	if (buffer == NULL)
+	}
+	(*buffer)[i] = '\0';
+	return (0);
+}
+
+int	get_temperature_seek(char **buffer)
+{
+	if (ft_strstr(buffer, "YES") != 0)
 		return (-503);
-	buffer = strstr(buffer, "t=");
-	if (buffer == NULL)
+	if (ft_strstr(buffer, "t=") != 0)
 		return (-504);
-	buffer += 2;
+	return (0);
+}
+
+int	get_temperature_get_number(char **str)
+{
+	int	i;
+
+	while (is_numb(**str) == 1)
+		(*str)++;
 	i = 0;
-	while (is_numb(buffer[i]) == 0)
+	while (is_numb((*str)[i]) == 0)
 		i++;
-	buffer[i] = '\0';
-	temperature = atof(buffer);
-	temperature /= 1000;
+	(*str)[i] = '\0';
+	return (i);
+}
+
+// Code erreur :
+// -500 ->
+// 
+// 
+// 
+//
+double	get_temperature(int id_capteur)
+{
+	double	temperature;
+	char	*buffer;
+	int	fd;
+	int	i;
+
+	buffer = malloc(100);
+	if (buffer == NULL)
+		return (-500);
+	if ((fd = get_temperature_open_file(&buffer, id_capteur)) < 0)
+		return (fd);
+	if ((i = get_temperature_read_file(&buffer, &fd)) != 0)
+		return (i);
+	if ((i = get_temperature_seek(&buffer)) != 0)
+		return (i);
+	i = get_temperature_get_number(&buffer);
+	temperature = atof(buffer) / 1000;
 	return (temperature);
 }
 
